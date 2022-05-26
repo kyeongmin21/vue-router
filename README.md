8c46055d3648468fe5fe0827532d2f3a1ef659d2
kim kyeong min <kyeongmin21@naver.com>
2022-05-26 20:28:50 +0900

    router-view 로 경로에 따른 페이지 보여지게 하기

--- README.md
+++ README.md
@@ -5,7 +5,14 @@
 * 맨 처음에 전체 컴포넌트를 라우터가 쥐고 있어야 함 > 처음 로딩 속도가 느려짐
 * lazy-loaded : 주소창에 값이 입력 됐을 때 이 값에 해당하는 컴포넌트 내용만 불러오겠다!
 
-### Compiles and hot-reloads for development
 
+### router-view 태그 
+* 페이지 표시 태그
+* url에 따른 컴포넌트가 화면에 그려지는 영역
+
+
+### @click="$router.push({ })"
+* router 에 값을 push 하겠다! 라는 뜻
+* 객체로 써주는 경우가 현업에서 많이 쓰인다.
 
 ### Compiles and minifies for production
--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,9 @@
     <v-navigation-drawer
       v-model="drawer"
       fixed
-      app
-    >
+      app>
       <v-list dense>
-        <v-list-tile>
+        <v-list-tile @click="$router.push({ name: 'home' })">
           <v-list-tile-action>
             <i class="fas fa-home"></i>
           </v-list-tile-action>
@@ -14,7 +13,8 @@
             <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile>
+
+        <v-list-tile @click="$router.push({ name: 'about' })">
           <v-list-tile-action>
             <i class="fas fa-user"></i>
           </v-list-tile-action>
@@ -28,9 +28,11 @@
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Application</v-toolbar-title>
     </v-toolbar>
-    <v-content>
 
+    <v-content>
+      <router-view></router-view>
     </v-content>
+
     <v-footer color="indigo" app>
       <span class="white--text">&copy; 2017</span>
     </v-footer>
@@ -44,6 +46,11 @@ export default {
   }),
   props: {
     source: String
+  },
+  methods: {
+    homeClick () {
+
+    }
   }
 }
 </script>
