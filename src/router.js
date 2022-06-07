3e12a9e13ebf0fd70e4ef02eb347efcd838f44aa
kim kyeong min <kyeongmin21@naver.com>
2022-06-07 19:32:45 +0900

    주석처리

--- src/router.js
+++ src/router.js
@@ -16,19 +16,16 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
       path: '/users',
       name: 'users',
-      beforeEnter: (to, from, next) => {
+/*      beforeEnter: (to, from, next) => {
         console.log('to', to)
         console.log('from', from)
         next('/')
-      },
+      },*/
       component: () => import(/* webpackChunkName: "users" */ './views/Users.vue'),
       children: [
         { path: ':id',
